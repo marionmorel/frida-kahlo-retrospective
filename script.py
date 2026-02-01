acec7a6e5b5ce2fab739f0af40300b14b3ef0906
marionmorel <marion.mrl@live.fr>
2026-02-01 17:18:57 +0000

    Task 4

--- script.py
+++ script.py
@@ -7,3 +7,9 @@ dates = [1939, 1933, 1946, 1940]
 # Task 3
 paintings = list(zip(paintings, dates))
 print(paintings)
+
+# Task 4
+paintings.append(("The Broken Column", 1944))
+paintings.append(("The Wounded Deer", 1946))
+paintings.append(("Me and My Doll", 1937))
+print(paintings)
