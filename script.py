6224661687273b9096e4043a1fb13bbff1f985c7
marionmorel <marion.mrl@live.fr>
2026-02-01 17:14:37 +0000

    Task 3

--- script.py
+++ script.py
@@ -3,3 +3,7 @@ paintings = ["The Two Fridas", "My Dress Hangs Here", "Tree of Hope", "Self Port
 
 # Task 2
 dates = [1939, 1933, 1946, 1940]
+
+# Task 3
+paintings = list(zip(paintings, dates))
+print(paintings)
